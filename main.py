a858f89b83a2f7407d21aa9cbce17b92da2cc462
Tsukii <kanin.twcr@gmail.com>
2023-03-23 18:13:04 +0700

    add screenshot feature with gray scale (this improve ocr result)

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ import cv2 as cv
 import numpy as np
 import pyautogui
 
-def ocr(imgName):
+def screenshort():
+    screenshort = pyautogui.screenshot()
+    screenshort = np.array(screenshort)
+    screenshort = cv.cvtColor(screenshort, cv.COLOR_RGB2GRAY)
+    return screenshort
+
+def ocr(image):
+    tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    # image = Image.open(imgName)
+    # Extract text from the image and split into a list
+    return tess.image_to_string(image).split("\n")
+
+# Temp func for develop
+def ocrFromFile(imgName): 
     tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     image = Image.open(imgName)
     # Extract text from the image and split into a list
@@ -24,20 +37,13 @@ def specifyMod(object):
         if "voidstone" in str(object[i]).lower():
             return object[i+1]
         
-def screenshort():
-    screenshort = pyautogui.screenshot()
-    screenshort = np.array(screenshort)
-    screenshort = cv.cvtColor(screenshort, cv.COLOR_RGB2BGR)
-
-    cv.imshow('comp', screenshort)
-    cv.waitKey()
-
 # Main program start here
-imgName = "lfDrop.jpg"
-screenshort()
-# readList = ocr(imgName)
-# cleanList = cleanSpace(readList)
-# mod = specifyMod(cleanList)
-# print(mod)
+imgName = "gray.jpg"
+img = screenshort()
+readList = ocr(img)
+# readList = ocrFromFile(imgName)
+cleanList = cleanSpace(readList)
+mod = specifyMod(cleanList)
+print(mod)
 
 
